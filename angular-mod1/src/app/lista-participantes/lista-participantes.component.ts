31af4deac0664ab8e780ca3b8425962c25164b3e
Priscila Sacchi <sacchipriscila@gmail.com>
2020-09-16 17:47:48 -0300

    Proyecto + Bootstrap + Componentes

---
+++ angular-mod1/src/app/lista-participantes/lista-participantes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-participantes',
+  templateUrl: './lista-participantes.component.html',
+  styleUrls: ['./lista-participantes.component.scss']
+})
+export class ListaParticipantesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
